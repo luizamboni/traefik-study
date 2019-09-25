ffc4ed47ba8bd394616dbd84b0a8e4b8f69a04cb
Luiz Carlos Zamboni <luiz.zamboni@zoom.com.br>
2019-09-25 16:05:23 -0300

    merge compose configs in multiple domains example

--- other-examples/multiple-domains/run.sh
+++ other-examples/multiple-domains/run.sh
@@ -1,9 +1,12 @@
 
 
-docker network create proxy
+docker network create proxy_net
 
 docker network create a_net
 
 docker network create b_net
 
-docker-compose -f traefik -f a_service.yaml -f b_service.yaml
+docker-compose -f a-compose.yml \
+               -f b-compose.yml \
+               -f traefik-compose.yml \
+               up
